783a482da52926c8a8f2d64aa9ecd60c05c4f025
Maria Eduarda de Araujo Freire <95583989+MariaE-duarda@users.noreply.github.com>
2022-07-30 14:11:36 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 150px;
+    text-align: center;
+    background: #395B64;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+    font-size: 50px;
+    padding-top: 20px;
+    color: white;
+`;
---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    
+    body {
+        font-family: 'Poppins', sans-serif;
+        background-color: #f0f0f0;
+    }
+`;
+
+export default Global; 
